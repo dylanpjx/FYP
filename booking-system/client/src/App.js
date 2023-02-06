01be754efe623406d9f033bb8b08b09ae2fa4c49
dylan <dylanpjx@gmail.com>
2023-02-06 20:24:17 +0800

    Refactor merge, move all board components to another directory

--- booking-system/client/src/App.js
+++ booking-system/client/src/App.js
@@ -6,6 +6,11 @@ import Login from './Login';
 import Homepage from './Homepage';
 import Calendar from './Calendar';
 
+import Artix7 from './interface/Artix7.js'
+import Zedboard from './interface/Zedboard.js'
+import Stm32 from './interface/Stm32.js'
+
+
 function App() {
     return (
         <div>
@@ -14,6 +19,9 @@ function App() {
                     <Route path="/" element={<Login />} />
                     <Route path="home" element={<Homepage />} />
                     <Route path="calendar" element={<Calendar />} />
+                    <Route path="artix" element={<Artix7 />} />
+                    <Route path="zedboard" element={<Zedboard />} />
+                    <Route path="stm32" element={<Stm32 />} />
                 </Routes>
             </Router>
         </div>
