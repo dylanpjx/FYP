717eda8bbabc8b2538dfb7ecf691b9b5b10120a2
dylan <dylanpjx@gmail.com>
2023-02-14 14:49:13 +0800

    Boilerplate for Register.js done

--- booking-system/client/src/Register.js
+++ booking-system/client/src/Register.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { 
+    Alert,
     Avatar,
     Box,
     Button,
-    Checkbox,
     Container,
     CssBaseline,
-    FormControlLabel,
     Grid,
     Link,
     TextField,
@@ -15,13 +15,74 @@ import {
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+const BACKEND_URL = 'http://localhost:5000';
 
-// TODO: handleSubmit
 const Register = () => {
     const theme = createTheme();
+    
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("");
+
+    const validateEmail = () => {
+        console.log("email");
+        if (!email) {
+            setEmailError("Email is required");
+            return false;
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            setEmailError("Email is invalid");
+            return false;
+        } else {
+            setEmailError("");
+            return true;
+        }
+    };
+
+    const validatePassword = () => {
+        console.log("pass");
+        if (!password) {
+            setPasswordError("Password is required");
+            return false;
+        } else if (password.length < 8) {
+            setPasswordError("Password must be at least 8 characters");
+            return false;
+        } else {
+            setPasswordError("");
+            return true;
+        }
+    }; 
+
+    const validateConfirmPassword = () => {
+        console.log("confirm");
+        if (!confirmPassword) {
+            setConfirmPasswordError("Please confirm your password");
+            return false;
+        } else if (password !== confirmPassword) {
+            setConfirmPasswordError("Passwords do not match");
+            return false;
+        } else {
+            setConfirmPasswordError("");
+            return true;
+        }
+    };
 
     const onRegister = async (e) => {
         e.preventDefault();
+        
+        if (validateEmail() && validatePassword() && validateConfirmPassword()) {
+            try {
+                const res = await axios.post(`${BACKEND_URL}/register`, {
+                    email, password
+                });
+                setMessage(res.data.message);
+            } catch (err) {
+                setMessage(err.response.data.message);
+            };
+        }
     }
 
     return (
@@ -43,29 +104,8 @@ const Register = () => {
                   <Typography component="h1" variant="h5">
                     Sign up
                   </Typography>
-                  <Box component="form" noValidate sx={{ mt: 3 }}> {/*TODO */}
+                  <Box component="form" noValidate onSubmit={onRegister} sx={{ mt: 3 }}>
                     <Grid container spacing={2}>
-                      <Grid item xs={12} sm={6}>
-                        <TextField
-                          autoComplete="given-name"
-                          name="firstName"
-                          required
-                          fullWidth
-                          id="firstName"
-                          label="First Name"
-                          autoFocus
-                        />
-                      </Grid>
-                      <Grid item xs={12} sm={6}>
-                        <TextField
-                          required
-                          fullWidth
-                          id="lastName"
-                          label="Last Name"
-                          name="lastName"
-                          autoComplete="family-name"
-                        />
-                      </Grid>
                       <Grid item xs={12}>
                         <TextField
                           required
@@ -74,8 +114,16 @@ const Register = () => {
                           label="Email Address"
                           name="email"
                           autoComplete="email"
+                          onChange={(e) => setEmail(e.target.value)}
                         />
+                      {
+                          emailError && 
+                          <Alert severity="error" variant="outlined">
+                              {emailError}
+                          </Alert>
+                      }
                       </Grid>
+
                       <Grid item xs={12}>
                         <TextField
                           required
@@ -85,7 +133,32 @@ const Register = () => {
                           type="password"
                           id="password"
                           autoComplete="new-password"
+                          onChange={(e) => setPassword(e.target.value)}
                         />
+                      {
+                          passwordError && 
+                          <Alert severity="error" variant="outlined">
+                              {passwordError}
+                          </Alert>
+                      }
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          name="repassword"
+                          label="Re-enter Password"
+                          type="password"
+                          id="repassword"
+                          autoComplete="new-password"
+                          onChange={(e) => setConfirmPassword(e.target.value)}
+                        />
+                      {
+                          confirmPasswordError && 
+                          <Alert severity="error" variant="outlined">
+                              {confirmPasswordError}
+                          </Alert>
+                      }
                       </Grid>
                     </Grid>
                     <Button
