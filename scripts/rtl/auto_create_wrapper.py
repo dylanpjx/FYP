6174c60d87891c84d3b91096165df5d2af3f89f9
dylan <dylanpjx@gmail.com>
2023-03-06 21:33:07 +0800

    Partial completion of auto_create_wrapper.py

--- scripts/rtl/auto_create_wrapper.py
+++ scripts/rtl/auto_create_wrapper.py
@@ -2,11 +2,29 @@
 ## usage: auto_create_wrapper.py <absolute_path/top_file.v>
 
 import sys
-top_file = sys.argv[0]
+top_file = sys.argv[1]
 
 print("Path to top file:", top_file)
 
-f = open(top_file)
-for l in f:
-    
+module_name = ""
+module_ports = []
 
+# parse top file line by line
+file = open(top_file, "r")
+
+get_ports = 0
+for line in file:
+    line = line.strip()
+
+    if (get_ports):
+        port = line.strip("input | output").strip(",")
+        print(port)
+        if line.startswith(");"):
+            break
+
+    if line.startswith("module"):
+        module_name = line.strip("module").strip("(").strip()
+        get_ports = 1
+
+
+# print (module_name, module_ports)
