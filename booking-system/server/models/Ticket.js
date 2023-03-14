587317cf77228382e2602f03c8b9b07a1352b97b
whitecompass <116876873+whitecompass@users.noreply.github.com>
2023-03-14 12:54:55 +0800

    Create Ticket model

---
+++ booking-system/server/models/Ticket.js
@@ -0,0 +1,25 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../connection')
+
+class Ticket extends Model {}
+
+Ticket.init({
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'Ticket',
+    timestamps: false
+});
+
+module.exports = Ticket;
