e98f170c51f705e420c4934af68e364cc8b3bfa6
whitecompass <116876873+whitecompass@users.noreply.github.com>
2023-03-14 13:12:41 +0800

    Create fileticket.js

---
+++ booking-system/server/fileticket.js
@@ -0,0 +1,20 @@
+const Ticket = require('./models/Ticket');
+
+const handleTicket = async (req, res) => {
+    const { name, email, description } = req.body;
+
+    try {
+        const newTicket = await Ticket.create({
+            name,
+            email,
+            description,
+        });
+        return res.status(201).send(`Ticket successfully filed.`);
+      
+    } catch (err) {
+        console.error('Error filing ticket.', err);
+        res.status(500).send('Internal server error');
+    }
+};
+
+exports.handleLogin = handleTicket;
