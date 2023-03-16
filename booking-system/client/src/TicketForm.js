bba305a3b6e505968ce146f4d4c615000bc275b7
whitecompass <116876873+whitecompass@users.noreply.github.com>
2023-03-16 16:39:31 +0800

    Added /ticketsuccess navigation

--- booking-system/client/src/TicketForm.js
+++ booking-system/client/src/TicketForm.js
@@ -30,6 +30,7 @@ const TicketForm = () => {
                 name, email, description
             });
             console.log(res.data);
+            return navigate('/ticketsucess');
             
         } catch (err) {
             console.error(err.response.data);
