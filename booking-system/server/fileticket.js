20ed5409568a619d815e31dd0d08b10acf4a7c78
whitecompass <116876873+whitecompass@users.noreply.github.com>
2023-03-18 17:50:45 +0800

    Added sending ticket to email

--- booking-system/server/fileticket.js
+++ booking-system/server/fileticket.js
@@ -1,4 +1,5 @@
 const Ticket = require('./models/Ticket');
+const nodemailer = require('nodemailer');
 
 const handleTicket = async (req, res) => {
     const { name, email, description, ticketType } = req.body;
@@ -8,8 +9,32 @@ const handleTicket = async (req, res) => {
             name,
             email,
             description,
-            ticketType,
+            ticketType
         });
+
+        //create reusable transporter object using default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'premises97@gmail.com',
+                pass: 'pdccwynezcorsumx'
+            }
+        });
+
+        //send email with defined transport object
+        let info = await transporter.sendMail({
+            from: 'premises97@gmail.com',
+            to: 'ticketsystem.fyp@gmail.com',
+            subject: 'New Ticket Filed',
+            html: `<p> A new ticket has been filed with the following details:</p>
+                  <p>Name: ${name}</p>
+                  <p>Email: ${email}</p>
+                  <p>Ticket Type: ${ticketType}</p>
+                  <p>Description: ${description}</p>`,
+        });
+
         return res.status(201).send(`Ticket successfully filed.`);
       
     } catch (err) {
