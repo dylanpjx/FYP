e3ff012418e982416f060fcf4a8a3d0d455f9465
Alvinci Merquita <alvinci.merquita@gmail.com>
2023-03-29 14:39:58 +0800

    added live feed, more instructions and sample codes

--- booking-system/client/src/interface/STM32.js
+++ booking-system/client/src/interface/STM32.js
@@ -5,6 +5,8 @@ import {
   Typography,
   Slider,
   Paper,
+  Tabs,
+  Tab,
   Container,
   Accordion,
   AccordionDetails,
@@ -16,7 +18,17 @@ import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import { useState, useContext, useEffect, useRef } from 'react';
 import { AuthContext } from '../AuthProvider';
-// import STM32Board from '../images/EE2028.PNG';
+import { getAcclerometerValues } from '../GetAccValues';
+import { 
+  sampleGPIOInit,
+  sampleGPIOCallback,
+  sampleUARTInit,
+  sampleUARTCode,
+  sampleUARTCodeSensor,
+  sampleI2CInit,
+  sampleI2CPinsInit,
+  sampleI2CCode
+} from '../SampleSTM32Codes'; 
 
 import LoadingScreen from './LoadingScreen';
 import NotAllowed from './NotAllowed';
@@ -26,7 +38,6 @@ const MODULE = 'EE2028';
 
 const sendSTM32 = async (buttonName, buttonState) => {
   try {
-    const token = localStorage.getItem('token');
     const response = await axios.post(
       BACKEND_URL+'/stm32', 
       { button: buttonName, value: buttonState }
@@ -37,41 +48,53 @@ const sendSTM32 = async (buttonName, buttonState) => {
   }
 };
 
-const cos = reading => {
-  return Math.cos(reading * Math.PI/180);
-}
+const CodeBlock = ({ code }) => {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(code);
+  };
 
-const sin = reading => {
-  return Math.sin(reading * Math.PI/180);
-}
+  return (
+    <Box
+      sx={{
+        bgcolor: "#f0f0f0",
+        p: 1,
+        borderRadius: 1,
+        position: "relative",
+      }}
+    >
+      <Typography
+        variant="body2"
+        component="code"
+        sx={{
+          whiteSpace: "pre-wrap",
+          fontFamily: "Menlo, monospace",
+        }}
+      >
+        {code}
+      </Typography>
+        <Button
+          variant="outlined"
+          size="small"
+          sx={{
+            position: "absolute",
+            top: 5,
+            right: 5,
+            fontSize: "0.8rem",
+            padding: "2px",
+            transition: "opacity 0.2s",
+            "&:hover": {
+              opacity: 1,
+              bgcolor: "white",
+            },
+          }}
+          onClick={handleCopy}
+        >
+          Copy
+        </Button>
+    </Box>
+  );
+};
 
-//a=yaw, B=pitch, y=roll
-const getAcclerometerValues = (a, B, y) => {
-  const gravitationalPull = [0,0,9.81];
-  const rotationMatrix = [
-  [cos(a)*cos(B), cos(a)*sin(B)*sin(y) - sin(a)*cos(y), cos(a)*sin(B)*cos(y) + sin(a)*sin(y)],
-  [sin(a)*cos(B), sin(a)*sin(B)*sin(y) + cos(a)*cos(y), cos(a)*sin(B)*sin(y) - cos(a)*sin(y)],
-  [-sin(B), cos(B)*sin(y), cos(B)*cos(y)]
-  ];
-
-  function multiply(mat1, mat2)
-  {
-    let res = [];
-    let temp = 0;
-      for (let i = 0; i < mat1.length; i++) {
-          for (let j = 0; j < mat2.length; j++) {
-              temp += mat1[i][j]*mat2[j];
-          }
-          res.push(Math.round(temp * 1000));
-          temp = 0;
-      }
-    return res;
-  }
-
-  const acc = multiply(rotationMatrix,gravitationalPull);
-  console.log(acc);
-  return acc;
-}
 
 const STM32 = () => {
   let navigate = useNavigate();
@@ -87,8 +110,13 @@ const STM32 = () => {
   const [start, setStart] = useState("");
   const [end, setEnd] = useState("");
   const [loading, setLoading] = useState(true);
+  const [tabValue, setTabValue] = useState(0);
 
   let intervalRef = useRef();
+
+  const handleTabChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
 
   const handlePitchChange = async (event, newValue) => {
     setPitch(newValue);
@@ -101,7 +129,6 @@ const STM32 = () => {
       console.log(err);
     }
   };
-
   const handleRollChange = async (event, newValue) => {
     setRoll(newValue);
     const acc = getAcclerometerValues(yaw,pitch,newValue);
@@ -129,7 +156,6 @@ const STM32 = () => {
     e.preventDefault();
     if (buttonCallback) buttonCallback(buttonName,1)
   }
-  
   const handleButtonRelease = (e,buttonName,buttonCallback) => {
     e.preventDefault();
     if (buttonCallback) buttonCallback(buttonName,0)
@@ -219,16 +245,17 @@ const STM32 = () => {
       >
           STM32
       </h3>
-      <p>Remaining time: {formatTime(remainingTime)}</p>
       <Container maxWidth={'xl'}>
         <Paper elevation={0} >
-          <Grid container spacing={2}>
+          <p>Remaining time: {formatTime(remainingTime)}</p>
+          <Grid container spacing={2} alignItems="stretch">
             <Grid item xs={8} align='center'>
-            {/* STM32 livefeed here */}
-            
-            {/* <img src={STM32Board} alt='STM32' width="300rem" height="350"/> */}
+            <Typography variant="subtitle1" align="center">
+                STM32 Livefeed
+            </Typography>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/3vDz4_xp6oM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item xs={4} >
               <Typography variant="subtitle1" align="center">
                 Board Buttons
               </Typography>
@@ -305,95 +332,156 @@ const STM32 = () => {
           </Grid>
         </Paper>
         <Paper elevation={0}>
-          <h3 align='center'>Guide to Remote STM32</h3>
-          
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography variant='subtitle2'>SSH Local Forwarding</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>We shall use SSH Local Forwarding to establish connection between STM32CubeIDE and OpenOCD as well as RealTerm and ser2net. OpenOCD (gdb server) runs on 3155 and ser2net runs on 3156 in our Remote Lab Worksatation. 
+        <Tabs value={tabValue} onChange={handleTabChange} centered>
+          <Tab label="Guide to Remote STM32" />
+          <Tab label="Virtual Buttons" />
+          <Tab label="Example Code" />
+        </Tabs>
+
+        <div hidden={tabValue !== 0}>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant='subtitle2'>SSH Local Forwarding</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>We shall use SSH Local Forwarding to establish connection between STM32CubeIDE and OpenOCD as well as RealTerm and ser2net. OpenOCD (gdb server) runs on 3155 and ser2net runs on 3156 in our Remote Lab Workstation. 
+              <br></br>
+              To begin forwarding
+            </Typography>
+            
+            <ol>
+              <li>Open your terminal (Powershell for Windows)</li>
+              <li>Enter this command to establish local forwarding 
+                <CodeBlock code='ssh -L 2000:localhost:3155 student@fpgaws2.panicker.in'></CodeBlock>
+                <br></br>
+                To bundle STM32CubeIDE &harr; OpenOCD and RealTerm &harr; ser2net connection in one command (if you are using UART in the board)
+                <CodeBlock code='ssh -L 2000:localhost:3155 -L 2001:localhost:3156 student@fpgaws2.panicker.in'></CodeBlock>
+                In your computer, STM32CubeIDE will connect to localhost:2000 for remote debugging and RealTerm will connect to localhost:2001 for serial interaction
+              </li>
+
+              <li>Leave the terminal open and it will close once your session is up</li>
+            </ol>
+
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant='subtitle2'>STM32 Debug Settings</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>Please apply these changes on STM32CubeIDE before starting remote debugging</Typography>
+            <ol>
+              <li>Right Click your project and Build Program</li>
+              <li>Right Click your Project &rarr; Debug As &rarr; Debug Configurations...</li>
+              <li>Debugger Tab
+                <br></br>
+                Under GDB Connection Settings, select "Connect to remote GDB server" 
+                <br></br>
+                Hostname or IP address = localhost. Port Number = 2000
+                <br></br>
+                Under "Misc" section, uncheck "Enable live expresion"
+              </li>
+              <li>
+                Click Apply and Debug
+              </li>
+            </ol>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant='subtitle2'>RealTerm</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>We shall establish RealTerm &harr; ser2net connection</Typography>
+            <ol>
+              <li>Click"Port" Tab</li>
+              <li>Set Baud to 115200 and Port as "localhost:2001" since we intialized 2001 for local forwarding to ser2net</li>
+              <li>Leave Parity, Data Bits, Stop Bits, Hardware Flow Control as it is</li>
+              <li>Click Open</li>
+            </ol>
+          </AccordionDetails>
+        </Accordion>
+        </div>
+
+        <div hidden={tabValue !== 1}>
+          <ol>
+            <li>Press and Release the "USER" button to virtualize a button press on the board. </li>
+            <li>Press the "RESET" button to trigger a reset.</li>
+            <li>Press the "ERASE" button to erase the program flashed on the board.</li>
+            <li>The three sliders are for adjusting the angles (pitch, roll, yaw) to simulate a rotation of an accelerometer sensor. These simulated readings can be obtained using an I2C Slave Board (example code on Example Code Tab &rarr; I2C)</li>
+          </ol>
+        </div>
+        <div hidden={tabValue !== 2}>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant='subtitle2'>GPIO</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>GPIO Pins Initialization for LED and Virtual Button (PC5) </Typography>
+            <CodeBlock code={sampleGPIOInit}/>
             <br></br>
-            To begin forwarding
-          </Typography>
-          
-          <ol>
-            <li>Open your terminal (Powershell for Windows)</li>
-            <li>Enter this command to establish local forwarding 
-              <Box sx={{ bgcolor: "#f0f0f0", p: 1, borderRadius: 1 }}>
-                <Typography variant="body2">
-                  <code>
-                    ssh -L 2000:localhost:3155 user@remotelab
-                  </code>
-                </Typography>
-              </Box>
-              <br></br>
-              To bundle STM32CubeIDE &harr; OpenOCD and RealTerm &harr; ser2net connection in one command (if you are using UART in the board)
-              
-              <Box sx={{ bgcolor: "#f0f0f0", p: 1, borderRadius: 1 }}>
-                <Typography variant="body2">
-                  <code>
-                    ssh -L 2000:localhost:3155 -L 2001:localhost:3156 user@remotelab
-                  </code>
-                </Typography>
-              </Box>
-              In your computer, STM32CubeIDE will connect to localhost:2000 for remote debugging and RealTerm will connect to localhost:2001 for serial interaction
-            </li>
-
-            <li>Leave the terminal open and it will close once your session is up</li>
-          </ol>
-
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography variant='subtitle2'>STM32 Debug Settings</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>Please apply these changes on STM32CubeIDE before starting remote debugging</Typography>
-          <ol>
-            <li>Right Click your project and Build Program</li>
-            <li>Right Click your Project &rarr; Debug As &rarr; Debug Configurations...</li>
-            <li>Debugger Tab
-              <br></br>
-              Under GDB Connection Settings, select "Connect to remote GDB server" 
-              <br></br>
-              Hostname or IP address = localhost. Port Number = 2000
-              <br></br>
-              Under "Misc" section, uncheck "Enable live expresion"
-            </li>
-            <li>
-              Click Apply and Debug
-            </li>
-          </ol>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography variant='subtitle2'>RealTerm</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>We shall establish RealTerm &harr; ser2net connection</Typography>
-          <ol>
-            <li>Click"Port" Tab</li>
-            <li>Set Baud to 115200 and Port as "localhost:2001" since we intialized 2001 for local forwarding to ser2net</li>
-            <li>Leave Parity, Data Bits, Stop Bits, Hardware Flow Control as it is</li>
-            <li>Click Open</li>
-          </ol>
-        </AccordionDetails>
-      </Accordion>
+            <Typography>GPIO Callback</Typography>
+            <CodeBlock code={sampleGPIOCallback}/>
+          </AccordionDetails>
+            
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant='subtitle2'>UART</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>UART Initialization</Typography>
+            <CodeBlock code={sampleUARTInit}></CodeBlock>
+            <br></br>
+            <Typography>UART Transmit</Typography>
+            <CodeBlock code={sampleUARTCode}></CodeBlock>
+            <br></br>
+            <Typography>UART Transmit Accelerometer Sensor (LSM6DSL) Data</Typography>
+            <CodeBlock code={sampleUARTCodeSensor}></CodeBlock>
+          </AccordionDetails>
+        </Accordion>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography variant='subtitle2'>I2C</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <h3>We will configure the physical pins (PB8 and PB9) as I2C and receive data from an I2C Slave Board (0x20). Please copy with caution.</h3>
+            <br></br>
+            <Typography>I2C Pins (PB8 and PB9) Initialization (Place these codes in Core/Src/stm32l4xx_hal_msp.c)</Typography>
+            <CodeBlock code={sampleI2CPinsInit}></CodeBlock>
+            <br></br>
+            <Typography>I2C Handler Initialization (Place these codes in Core/Src/main.c)</Typography>
+            <CodeBlock code={sampleI2CInit}></CodeBlock>
+            <br></br>
+            <Typography>I2C Read From Simulated Sensor with Slave Address of 0x20</Typography>
+            <CodeBlock code={sampleI2CCode}></CodeBlock>
+          </AccordionDetails>
+        </Accordion>
+        </div>
         </Paper>
       </Container>
     </main>
